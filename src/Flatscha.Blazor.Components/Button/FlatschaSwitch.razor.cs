0d0268d43f9027f98bbbc54e7812f69446aca47c
Samuel Flatscher <s.flatscher@metalldeutsch.com>
2024-01-22 15:08:55 +0100

    added flatschaswitch

---
+++ src/Flatscha.Blazor.Components/Button/FlatschaSwitch.razor.cs
@@ -0,0 +1,31 @@
+namespace Flatscha.Blazor.Components.Button
+{
+    public partial class FlatschaSwitch
+    {
+        private bool _checked;
+
+        [Parameter]
+        public bool Checked
+        {
+            get => this._checked;
+            set
+            {
+                if (this._checked == value) { return; }
+
+                this._checked = value;
+                this.CheckedChanged.InvokeAsync(value);
+            }
+        }
+
+        [Parameter] public EventCallback<bool> CheckedChanged { get; set; }
+
+        [Parameter] public string Label { get; set; }
+
+        private async Task Clicked()
+        {
+            this.Checked = !this.Checked;
+            await this.InvokeAsync(StateHasChanged);
+        }
+
+    }
+}
