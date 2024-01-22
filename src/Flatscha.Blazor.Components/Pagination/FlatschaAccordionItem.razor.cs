d0b09137b4d5f38082119f3cca3d3df5416bb7f4
Samuel Flatscher <s.flatscher@metalldeutsch.com>
2024-01-22 16:19:20 +0100

    added accordion

---
+++ src/Flatscha.Blazor.Components/Pagination/FlatschaAccordionItem.razor.cs
@@ -0,0 +1,28 @@
+namespace Flatscha.Blazor.Components.Pagination
+{
+    public partial class FlatschaAccordionItem
+    {
+        [CascadingParameter] protected FlatschaAccordion Parent { get; set; }
+
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        [Parameter] public string Name { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.Parent.Items.Add(this);
+            base.OnInitialized();
+        }
+
+        private async Task SetActive()
+        {
+            await this.Parent.SetActiveItem(this);
+            await this.InvokeAsync(StateHasChanged);
+        }
+
+        public void Dispose()
+        {
+            this.Parent.Items.Remove(this);
+        }
+    }
+}
