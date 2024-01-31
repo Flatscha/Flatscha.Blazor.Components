32e97be52cb3ed9093f24af8970f9dce74aad8ce
Samuel Flatscher <s.flatscher@metalldeutsch.com>
2024-01-31 08:22:20 +0100

    added side widget tests

---
+++ test/Flatscha.Blazor.Components.Tests/Widget/SideWidgetTests.cs
@@ -0,0 +1,33 @@
+﻿using Flatscha.Blazor.Components.Tests.Base;
+using Flatscha.Blazor.Components.Widget;
+
+namespace Flatscha.Blazor.Components.Tests.Widget
+{
+    public class SideWidgetTests : BaseMultiChildComponentTest<FlatschaSideWidget, CascadingFlatschaSideWidget>
+    {
+        private const string ClickElementSelector = ".flatscha-side-widget .flatscha-icon-button";
+
+        public SideWidgetTests()
+        {
+        }
+
+        [Fact]
+        public void Activate()
+        {
+            FlatschaSideWidget activeStep = null;
+
+            var (widgetContainer, widget1, widget2) = this.RenderParentWithChilds(
+                p => { p.Add(p => p.ActiveItemChanged, (item) => activeStep = item); });
+
+            widgetContainer.Render();
+
+            widget1.Find(ClickElementSelector).Click();
+            Assert.NotNull(activeStep);
+            Assert.Equal(widget1.Instance, activeStep);
+
+            widget2.Find(ClickElementSelector).Click();
+            Assert.NotNull(activeStep);
+            Assert.Equal(widget2.Instance, activeStep);
+        }
+    }
+}
