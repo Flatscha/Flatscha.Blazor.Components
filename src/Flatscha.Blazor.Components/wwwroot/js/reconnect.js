ce12f32b0778fb0c12227af386c78f1194482335
Samuel Flatscher <s.flatscher@metalldeutsch.com>
2025-04-04 09:05:44 +0200

    added better blazor reconnect

---
+++ src/Flatscha.Blazor.Components/wwwroot/js/reconnect.js
@@ -0,0 +1,57 @@
+﻿initReconnect();
+
+function initReconnect() {
+    if (Blazor.defaultReconnectionHandler == undefined) {
+        setTimeout(() => initReconnect(), 20);
+    }
+    else {
+        Blazor.defaultReconnectionHandler.onConnectionDown = function (options, error) {
+
+            display();
+            setTimeout(() => reconnect(), 5000);
+        };
+    }
+}
+
+function reconnect() {
+    var status = 0;
+
+    try {
+        var http = new XMLHttpRequest();
+        http.open("GET", window.location.origin, false);
+        http.send(null);
+        status = http.status;
+    }
+    catch { }
+
+    if (status == 200) {
+        document.location.reload(true);
+        return;
+    }
+
+    setTimeout(() => reconnect(), 5000);
+}
+
+function display() {
+    var modal = document.createElement('div');
+
+    const modalStyles = [
+        'position: fixed',
+        'top: 0',
+        'right: 0',
+        'bottom: 0',
+        'left: 0',
+        'z-index: 1050',
+        'display: block',
+        'overflow: hidden',
+        'background-color: #fff',
+        'opacity: 0.8',
+        'text-align: center',
+        'padding : 1rem',
+    ];
+
+    modal.style.cssText = modalStyles.join(';');
+    modal.innerHTML = 'Reconnecting...';
+
+    document.body.appendChild(modal);
+}
